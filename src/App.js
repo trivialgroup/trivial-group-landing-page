b9e3997330c6556b905f653ea869e9a50edb5c47
Frenk <fd5532@student.uni-lj.si>
2019-09-24 18:27:17 +0200

    Updated the logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './trivial-group-logo-mockup_Dark.svg';
+import { ReactComponent as Logo } from './component/images/trivial-logo-13_Dark.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Logo className="app-logo"/>
         <h1 className="trivial-group-header">
           Trivial Group
         </h1>
