4da701585ecf8d9a077eb180168bbd6aa23c3a6c
Frenk <fd5532@student.uni-lj.si>
2019-09-24 18:26:47 +0200

    Updates

---
+++ precache-manifest.8dd30938a9a89dccaaebace89885df61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d593b85641e4673ce4e57141d1a27521",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff36b4fbb0e9ef6a93b6",
+    "url": "/static/css/main.c1ebe332.chunk.css"
+  },
+  {
+    "revision": "9868ea86026e0f02b1fd",
+    "url": "/static/js/2.dc08d3e8.chunk.js"
+  },
+  {
+    "revision": "ff36b4fbb0e9ef6a93b6",
+    "url": "/static/js/main.61d704a6.chunk.js"
+  },
+  {
+    "revision": "f242361d4919c89cc6dd",
+    "url": "/static/js/runtime-main.e32326b8.js"
+  },
+  {
+    "revision": "2b373f7aeb07514ff9e533613abfd6e2",
+    "url": "/static/media/trivial-logo-13_Dark.2b373f7a.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7d93c43ed20516e359abd2b07d9bbb08.js"
+  "/precache-manifest.8dd30938a9a89dccaaebace89885df61.js"
 );
 
 self.addEventListener('message', (event) => {
